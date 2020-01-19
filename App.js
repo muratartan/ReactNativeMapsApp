1a096d5cdc935bfde6edad29d567cf2c5337512c
murat <muratartan@hotmail.com>
2020-01-19 14:23:18 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class mapApp extends React.Component {
+
+  render() {
+    return(
+
+      <View>
+        <Text>Salam</Text>
+      </View>
+    );
+  }
+}
+
+export default mapApp;
